229471f1f8476b65472bfddc9418ece7f6d3606d
Megan <iamdun24@gmail.com>
2022-06-28 09:28:58 -0400

    video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import Video from "./Video";
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Lets build a full stack mern tiktok clone. </h1>
+      
+      <Video />
+      
+      
+      {/* app container */}
+        {/* videos */}
+        {/* Video */}
+        {/* Video */} 
+        {/* Video */} 
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+        <video 
+        className="video_player"
+        loop
+        src="https://v16m-webapp.tiktokcdn-us.com/0788034cdf20881700254f3a29033929/62bb521d/video/tos/useast5/tos-useast5-pve-0068-tx/76094cc840ab472db0b3c95037cec2e1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C0%7C0&br=2778&bt=1389&cs=0&ds=3&ft=ebtHKH-qMyq8ZM-z8we2Ni5yfl7Gb&mime_type=video_mp4&qs=0&rc=NTc0OTg0aDw0ZTdlOjtpOUBpanh3bTM6ZjNvPDMzZzczNEAuL140MTE2NjAxMTBeLV5eYSNhMWpicjRvYzJgLS1kMS9zcw%3D%3D&l=20220628131006010004004007735002028030A4F5C"></video>
+    </div>
+
+
+  )
+}
+
+export default Video;
